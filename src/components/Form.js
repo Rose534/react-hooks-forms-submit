37ad194c1da6db8f11db94738b5f87fb3f3cdb7a
rose534 <wambuirose534@gmail.com>
2023-01-13 19:04:31 +0300

    form

--- src/components/Form.js
+++ src/components/Form.js
@@ -44,10 +44,17 @@ function Form() {
         <input type="text" onChange={handleLastNameChange} value={lastName} />
         <button type="submit">Submit</button>
       </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
       <h3>Submissions</h3>
       {listOfSubmissions}
     </div>
-  );
-}
+  );}
 
 export default Form;
